9e465e5b0879368e339c5d2180b48eca5151330b
miikaoskari <miika.karjalainen13@gmail.com>
2025-03-25 21:51:26 +0200

    initial work

---
+++ cs2kernel/main.c
@@ -0,0 +1,15 @@
+#include <ntifs.h>
+
+// API function from ntoskrnl.exe which we use
+// to copy memory to and from an user process.
+NTSTATUS NTAPI MmCopyVirtualMemory
+(
+    PEPROCESS SourceProcess,
+    PVOID SourceAddress,
+    PEPROCESS TargetProcess,
+    PVOID TargetAddress,
+    SIZE_T BufferSize,
+    KPROCESSOR_MODE PreviousMode,
+    PSIZE_T ReturnSize
+);
+
