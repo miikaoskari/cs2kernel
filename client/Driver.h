913acbd5cdf65d0505ef750702edf30a078f56bb
miikaoskari <miika.karjalainen13@gmail.com>
2025-04-14 19:42:58 +0300

    Fixes driver class template function

--- client/Driver.h
+++ client/Driver.h
@@ -1,6 +1,16 @@
 #pragma once
 
 #include <Windows.h>
+#include <iostream>
+
+#define IOCTL_READ_REQUEST CTL_CODE(FILE_DEVICE_UNKNOWN, 0x0701, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+
+typedef struct _KERNEL_READ_REQUEST {
+	ULONG pid;
+	ULONG64 address;
+	PVOID buffer;
+	SIZE_T size;
+} KERNEL_READ_REQUEST;
 
 class Driver
 {
@@ -13,7 +23,21 @@ public:
 	void closeDriver();
 	bool isDriverLoaded();
 
-	template<typename T> T ReadMemory(ULONG pid, uintptr_t address);
+	template<typename T> 
+	T ReadMemory(ULONG pid, uintptr_t address) {
+		T value{};
+
+		if (!isDriverLoaded()) {
+			std::cerr << "Driver not loaded." << std::endl;
+			return false;
+		}
+
+		KERNEL_READ_REQUEST req = { pid, address, &value, sizeof(T) };
+		DWORD bytesReturned;
+		DeviceIoControl(hDriver, IOCTL_READ_REQUEST, &req, sizeof(req), &req, sizeof(req), &bytesReturned, nullptr);
+
+		return value;
+	}
 
 private:
 	HANDLE hDriver;
