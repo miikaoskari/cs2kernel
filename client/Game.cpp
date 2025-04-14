654edcefdb4e16dbd981e3f5bb9ce5e9dbadeeb4
miikaoskari <miika.karjalainen13@gmail.com>
2025-04-15 01:52:35 +0300

    Adds game class for reading game entitylist and others

---
+++ client/Game.cpp
@@ -0,0 +1,38 @@
+#include "Game.h"
+#include "Driver.h"
+#include "Process.h"
+
+#include "offsets.hpp"
+#include "client_dll.hpp"
+
+using namespace cs2_dumper::offsets::client_dll;
+using namespace cs2_dumper::offsets::engine2_dll;
+using namespace cs2_dumper::schemas::client_dll;
+
+
+void Game::ReadData()
+{
+	auto& d = Driver::getInstance();
+
+	if (!d.isDriverLoaded())
+	{
+		std::cerr << "Driver not loaded." << std::endl;
+		return;
+	}
+
+	auto& p = Process::getInstance();
+
+	if (p.pid == 0)
+	{
+		std::cerr << "Process ID is not set." << std::endl;
+		return;
+	}
+
+	buildNumber = d.ReadMemory<uintptr_t>(p.pid, p.engineBaseAddress + dwBuildNumber);
+	plantedC4 = d.ReadMemory<uintptr_t>(p.pid, p.clientBaseAddress + dwPlantedC4);
+	weaponC4 = d.ReadMemory<uintptr_t>(p.pid, p.clientBaseAddress + dwWeaponC4);
+	entityList = d.ReadMemory<uintptr_t>(p.pid, p.clientBaseAddress + dwEntityList);
+	gameRules = d.ReadMemory<uintptr_t>(p.pid, p.clientBaseAddress + dwGameRules);
+	globalVars = d.ReadMemory<uintptr_t>(p.pid, p.clientBaseAddress + dwGlobalVars);
+	
+}
---
+++ client/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+class Game
+{
+public:
+	char map_name[128] = { 0 };
+
+	uintptr_t buildNumber;
+	uintptr_t plantedC4;
+	uintptr_t weaponC4;
+	uintptr_t entityList;
+	uintptr_t gameRules;
+	uintptr_t globalVars;
+	uintptr_t cameraServices;
+
+	float flashDuration;
+
+	int entityIndex;
+
+
+	void ReadData();
+};
+
