263d49565bd1a04652f6f89e35cbe38a27ca0118
miikaoskari <miika.karjalainen13@gmail.com>
2025-04-14 23:32:24 +0300

    Utilizes singletons in main loop

--- client/main.cpp
+++ client/main.cpp
@@ -18,7 +18,7 @@
 int main() {	
     const WCHAR* deviceName = L"\\\\.\\IoctlTest";
 
-	Driver driver;
+	auto& driver = Driver::getInstance();
 	driver.initDriver(deviceName);
 
 	if (!driver.isDriverLoaded()) {
@@ -26,56 +26,28 @@ int main() {
 		return 1;
 	}
 
-	Process process;
-	ULONG pid = process.FindProcessId("cs2.exe");
+	auto& process = Process::getInstance();
+	process.getAllBaseAddresses();
+	
 
-	if (pid == 0) {
-		std::cerr << "Failed to find cs2.exe process." << std::endl;
-		return 1;
-	}
-
-	std::cout << "Found cs2.exe process with PID: " << pid << std::endl;
-
-    uintptr_t clientBaseAddress = process.GetModuleBaseAddress(pid, L"client.dll");
-    if (clientBaseAddress == 0)
-    {
-        std::cerr << "Failed to find client.dll base address." << std::endl;
-        return 1;
-    }
-
-	uintptr_t engine2BaseAddress = process.GetModuleBaseAddress(pid, L"engine2.dll");
-	if (engine2BaseAddress == 0)
-	{
-		std::cerr << "Failed to find engine2.dll base address." << std::endl;
-		return 1;
-	}
-
-	uintptr_t matchmakingBaseAddress = process.GetModuleBaseAddress(pid, L"matchmaking.dll");
-	if (matchmakingBaseAddress == 0)
-	{
-		std::cerr << "Failed to find matchmaking.dll base address." << std::endl;
-		return 1;
-	}
-
-    std::cout << "client.dll base address: " << std::hex << clientBaseAddress << std::endl;
-	std::cout << "engine2.dll base address: " << std::hex << engine2BaseAddress << std::endl;
-	std::cout << "matchmaking.dll base address: " << std::hex << matchmakingBaseAddress << std::endl;
+    std::cout << "client.dll base address: " << std::hex << process.clientBaseAddress << std::endl;
+	std::cout << "engine2.dll base address: " << std::hex << process.engineBaseAddress << std::endl;
+	std::cout << "matchmaking.dll base address: " << std::hex << process.matchmakingBaseAddress << std::endl;
 
 
-    uintptr_t playerPawnAddress = clientBaseAddress + cs2_dumper::offsets::client_dll::dwLocalPlayerPawn;
-    uintptr_t entityListAddress = clientBaseAddress + cs2_dumper::offsets::client_dll::dwEntityList;
-
+    uintptr_t playerPawnAddress = process.clientBaseAddress + cs2_dumper::offsets::client_dll::dwLocalPlayerPawn;
+    uintptr_t entityListAddress = process.clientBaseAddress + cs2_dumper::offsets::client_dll::dwEntityList;
 
     for(;;)
     {
-		uintptr_t playerPawn = driver.ReadMemory<uintptr_t>(pid, playerPawnAddress);
-		uintptr_t entityList = driver.ReadMemory<uintptr_t>(pid, entityListAddress);
+		uintptr_t playerPawn = driver.ReadMemory<uintptr_t>(process.pid, playerPawnAddress);
+		uintptr_t entityList = driver.ReadMemory<uintptr_t>(process.pid, entityListAddress);
 
         if (playerPawn != NULL)
         {
-            int team = driver.ReadMemory<int>(pid, (playerPawn + cs2_dumper::schemas::client_dll::C_BaseEntity::m_iTeamNum));
-			int health = driver.ReadMemory<int>(pid, (playerPawn + cs2_dumper::schemas::client_dll::C_BaseEntity::m_iHealth));
-			int armor = driver.ReadMemory<int>(pid, (playerPawn + cs2_dumper::schemas::client_dll::C_CSPlayerPawn::m_ArmorValue));
+            int team = driver.ReadMemory<int>(process.pid, (playerPawn + cs2_dumper::schemas::client_dll::C_BaseEntity::m_iTeamNum));
+			int health = driver.ReadMemory<int>(process.pid, (playerPawn + cs2_dumper::schemas::client_dll::C_BaseEntity::m_iHealth));
+			int armor = driver.ReadMemory<int>(process.pid, (playerPawn + cs2_dumper::schemas::client_dll::C_CSPlayerPawn::m_ArmorValue));
 
 			std::cout << "Player Team: " << team << std::endl;
 			std::cout << "Player Health: " << health << std::endl;
