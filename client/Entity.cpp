f9cbc32191aa01875dc4184eaad099b4d29a3238
miikaoskari <miika.karjalainen13@gmail.com>
2025-04-26 18:42:35 +0300

    Adds entityclass for reading entities

---
+++ client/Entity.cpp
@@ -0,0 +1,50 @@
+#include "Entity.h"
+#include "Driver.h"
+#include "Process.h"
+
+#include "offsets.hpp"
+#include "client_dll.hpp"
+
+using namespace cs2_dumper::offsets::client_dll;
+using namespace cs2_dumper::schemas::client_dll;
+
+void Entity::ReadData(uintptr_t playerPawn, uintptr_t playerController)
+{
+	auto& d = Driver::getInstance();
+
+	if (!d.isDriverLoaded())
+	{
+		std::cerr << "Driver not loaded." << std::endl;
+		return;
+	}
+
+	auto& p = Process::getInstance();
+
+	if (p.pid == 0)
+	{
+		std::cerr << "Process ID is not set." << std::endl;
+		return;
+	}
+
+	if (playerPawn != NULL)
+	{
+		team = d.ReadMemory<int>(p.pid, (playerPawn + C_BaseEntity::m_iTeamNum));
+		health = d.ReadMemory<int>(p.pid, (playerPawn + C_BaseEntity::m_iHealth));
+		armor = d.ReadMemory<int>(p.pid, (playerPawn + C_CSPlayerPawn::m_ArmorValue));
+		aimPunchAngle = d.ReadMemory<float>(p.pid, (playerPawn + C_CSPlayerPawn::m_aimPunchAngle));
+		isScoped = d.ReadMemory<bool>(p.pid, (playerPawn + C_CSPlayerPawn::m_bIsScoped));
+	}
+
+	if (playerController != NULL)
+	{
+		uintptr_t nameAddress = d.ReadMemory<uintptr_t>(p.pid, (playerController + CCSPlayerController::m_sSanitizedPlayerName));
+		if (nameAddress != NULL)
+		{
+			// TODO: implement ReadMemoryArray to read the name
+			// d.ReadMemory<char>(p.pid, nameAddress);
+		}
+		ping = d.ReadMemory<int>(p.pid, (playerController + CCSPlayerController::m_iPing));
+	}
+
+	return;
+}
---
+++ client/Entity.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstddef>
+
+class Entity
+{
+public:
+	uintptr_t entity;
+	uintptr_t controller;
+
+	char name[128] = { 0 };
+	int team;
+	int health;
+	int armor;
+	int ping;
+
+	float aimPunchAngle;
+	float sensitivity;
+
+	bool isScoped;
+
+	void ReadData(uintptr_t playerPawn, uintptr_t playerController);
+
+};
+
